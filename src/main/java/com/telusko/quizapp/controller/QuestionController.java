7edea2f92ec257b609dbb1e5fce0889fc032b7bd
ronald sarmiento <ronisarmont@gmail.com>
2023-10-06 22:41:43 -0400

    get All Questions / ByCategory - QuestionController Question QuestionService and Question Dao Created

---
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -0,0 +1,28 @@
+package com.telusko.quizapp.controller;
+
+import com.telusko.quizapp.Question;
+import com.telusko.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+}
