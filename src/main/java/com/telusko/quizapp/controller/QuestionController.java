2e96014342085c9ccadee8009e52113ee0a40f6f
ronald sarmiento <ronisarmont@gmail.com>
2023-10-08 18:22:04 -0400

    Question Service addQuestion

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -3,10 +3,7 @@ package com.telusko.quizapp.controller;
 import com.telusko.quizapp.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,11 @@ public class QuestionController {
 
     @GetMapping("category/{category}")
     public List<Question> getQuestionsByCategory(@PathVariable String category){
-        return questionService.getQuestionsByCategory(category);
+      return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+      return questionService.addQuestion(question);
     }
 }
