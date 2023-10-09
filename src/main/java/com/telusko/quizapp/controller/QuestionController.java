b43c2a7e832855d33df141aba0b249984fb347a3
ronald sarmiento <ronisarmont@gmail.com>
2023-10-08 22:34:54 -0400

    Quiz table created

--- src/main/java/com/telusko/quizapp/controller/QuestionController.java
+++ src/main/java/com/telusko/quizapp/controller/QuestionController.java
@@ -1,9 +1,8 @@
 package com.telusko.quizapp.controller;
 
-import com.telusko.quizapp.Question;
+import com.telusko.quizapp.model.Question;
 import com.telusko.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
