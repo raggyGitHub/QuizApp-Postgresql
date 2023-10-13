dc1206b621fdc13a9e83b036ef3daac0ec901b6e
ronald sarmiento <ronisarmont@gmail.com>
2023-10-13 16:09:16 -0400

    Response Entity completed

--- src/main/java/com/telusko/quizapp/controller/QuizController.java
+++ src/main/java/com/telusko/quizapp/controller/QuizController.java
@@ -1,13 +1,16 @@
 package com.telusko.quizapp.controller;
 
+import com.telusko.quizapp.model.Question;
+import com.telusko.quizapp.model.QuestionWrapper;
+import com.telusko.quizapp.model.Response;
 import com.telusko.quizapp.service.QuizService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,4 +22,27 @@ public class QuizController {
                                              @RequestParam String title){
         return quizService.createQuiz(category,numQ,title);
     }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id,responses);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/telusko/quizapp/service/QuizService.java
+++ src/main/java/com/telusko/quizapp/service/QuizService.java
@@ -1,15 +1,19 @@
 package com.telusko.quizapp.service;
 
+import com.telusko.quizapp.model.QuestionWrapper;
 import com.telusko.quizapp.model.Quiz;
 import com.telusko.quizapp.dao.QuestionDao;
 import com.telusko.quizapp.model.Question;
 import com.telusko.quizapp.dao.QuizDao;
+import com.telusko.quizapp.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -26,5 +30,32 @@ public class QuizService {
         quiz.setQuestions(questions);
         quizDao.save(quiz);
         return  new ResponseEntity<>("Success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser  =new ArrayList<>();
+        for(Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(),q.getQuestionTitle(),q.getOption1(),
+                    q.getOption2(),q.getOption3(), q.getOption4());
+            questionForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionForUser,HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right=0;
+        int i=0;
+        for (Response response:responses){
+            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
+                right++;
+
+            i++;
+        }
+        return new ResponseEntity<>(right,HttpStatus.OK);
     }
 }
